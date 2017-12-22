ef1d89f47bf792908368d38e2a3b198c9b39e907
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-12-22 22:46:22 +0700

    upload code

---
+++ countdown/countdown/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace countdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.countdown_time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // countdown_time
+            // 
+            this.countdown_time.AutoSize = true;
+            this.countdown_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 159.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.countdown_time.Location = new System.Drawing.Point(-1, 110);
+            this.countdown_time.Name = "countdown_time";
+            this.countdown_time.Size = new System.Drawing.Size(936, 241);
+            this.countdown_time.TabIndex = 0;
+            this.countdown_time.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 484);
+            this.Controls.Add(this.countdown_time);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "นับถอยหลัง";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label countdown_time;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ countdown/countdown/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace countdown
+{
+    public partial class Form1 : Form
+    {
+        double settime = 0;
+        bool run;
+        DateTime time = DateTime.Now;
+        DateTime t1 = DateTime.Now;
+        DateTime t2 = new DateTime(2017, 12, 25, 9, 0, 0);
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (run != false && settime > 0) // กรณี run ไม่เป็น false และ settime มากกว่า 0
+            {
+                settime--; // ให้ลบค่า 1 ค่าใน settime
+                countdown_time.Text= string.Format("{0:00}:{1:00}:{2:00}", settime / 3600, (settime / 60) % 60, settime % 60); // แปลงเป็น ชั่วโมง นาที วินาที
+                if (settime <= 0) // กรณี settime เท่ากับ 0
+                {
+                    timer1.Stop(); // ให้หยุดการทำงาน timer1
+                    run = false; // ให้ run เป็น false
+                    timer1.Enabled = false; // ให้ปิดการใช้งาน timer1
+                }
+            }
+            else // กรณีไม่เข้าเงื่อนไขข้างบน
+            {
+                countdown_time.Text = "00:00:00"; //ให้ข้อความของ label2 เป็น  "00:00:00"
+                run = false; // ให้ run เป็น false
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            run = true; // ให้ run เป็น true
+            double diffInSeconds = (t2 - t1).TotalSeconds; // หาความความต่างเวลาในหน่วยวินาที
+            settime = diffInSeconds; //h*60*60+m*60+s;
+            if (settime != 0) // ถ้า settime ไม่เป็น 0
+            {
+                timer1.Enabled = true; // ให้เปิดการใช้งาน timer1
+                timer1.Start(); // ให้เริ่มการทำงาน timer1
+            }
+        }
+    }
+}
